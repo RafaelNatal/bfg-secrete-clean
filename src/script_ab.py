adb77b115a18cd57fed0676980fa7b4e64017950
Rafael Natal <rafael.natal@hotmail.com>
2024-05-21 22:30:48 -0300

    ab file

---
+++ src/script_ab.py
@@ -0,0 +1,14 @@
+user = '***REMOVED***b'
+passwd = '***REMOVED***'
+
+
+# user ***REMOVED***
+# passwd ***REMOVED***
+
+
+if __name__ == "__main__":
+    print("================================")
+    print("Hello")
+    print("================================")
+    print(f'user: {user}')
+    print(f'pass: {passwd}')
